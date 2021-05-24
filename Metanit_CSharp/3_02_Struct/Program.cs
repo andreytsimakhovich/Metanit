3007212a6d84d62a59789fb25bdd43f282475e5b
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-05-24 10:56:39 +0300

    Add 3_02_Struct.

---
+++ Metanit_CSharp/3_02_Struct/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_02_Struct
+{
+    class Program
+    {
+        struct User
+        {
+            public string name;
+            public int age;
+
+            public User(string name, int age)  // Определение конструктора в структуре
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine($"Name: {name},  Age: {age}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            User user;
+
+            Console.WriteLine("Enter your name:");
+            user.name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            user.age = int.Parse(Console.ReadLine());
+
+            user.Display();
+
+            User tom = new User();  // При использовании конструктора по умолчанию не надо явным образом инициализировать поля структуры.
+            tom.Display();
+
+            User bob = new User("Bob", 33);  //  При определении конструктора в структуре необходимо инициализировать все поля
+            bob.Display();
+
+
+            User person = new User { name = "Sam", age = 31 };   // Использование инициализатора для создания структуры
+            person.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Metanit
 Analysis of examples on the site metanit.com
+
+### Глава 3. Классы. Объектно-ориентированнное программирование. ###
+
+* 3_02_Структуры
